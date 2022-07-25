11482eee39f1a4ea8aa6973fb98b65b1109b5a19
Aleksandar204 <aleksandar.r204@gmail.com>
2022-07-25 13:24:21 +0200

    Pocetak audio sistema

--- main.py
+++ main.py
@@ -3,14 +3,19 @@ from PySide2.QtUiTools import QUiLoader
 from PySide2.QtWidgets import QApplication,QFileDialog,QMessageBox
 from PySide2.QtCore import QFile
 
-import pyaudio
-import wave
-import time
-    
+import numpy as np
+from scipy.io.wavfile import read
+from scipy.io.wavfile import write
+import sounddevice as sd
+
 
 class MainWindow():
     fileName = ""
     playing = False
+    stream = None
+    fs = 0
+    x = None
+
     def __init__(self):
         ui_file_name = "ui_files/mainwindow.ui"
         ui_file = QFile(ui_file_name)
@@ -22,25 +27,30 @@ class MainWindow():
         self.win.menuHelp.triggered.connect(self.menuAboutEvent)
         self.win.show()
 
+    def menuAboutEvent(self):
+        QMessageBox.about(None,"PFE Autotune","Napravio Aleksandar Rašković za vreme prolećne online PFE radionice")
+    def menuFileEvent(self,args):
+        val = args.text()
+        if val == 'Open':
+            self.fileName = QFileDialog.getOpenFileName(None,"Open Image", "./", "Audio Files (*.wav)")[0]
+            self.fs,self.x = read(self.fileName)
+        if val == 'Export':
+            write("output.wav",self.fs,self.x)
+        if val == 'Close':
+            self.fileName = ""
+            self.fs = 0
+            self.x = None
+        elif val == 'Exit':
+            QApplication.quit()
+
     def play(self):
         self.playing = not self.playing
         if not self.playing:
             self.win.pushButton.setText("Play")
+            sd.stop()
         else:
             self.win.pushButton.setText("Pause")
-
-    def menuAboutEvent(self):
-        QMessageBox.about(None,"PFE Autotune","Napravio Aleksandar Rašković za vreme prolećne online radionice")
-    def menuFileEvent(self,args):
-        val = args.text()
-        if val == 'Open':
-            fileName = QFileDialog.getOpenFileName(None,"Open Image", "./", "Audio Files (*.wav)")[0]
-            print(fileName)
-        if val == 'Close':
-            fileName = ""
-        elif val == 'Exit':
-            QApplication.quit()
-
+            sd.play(self.x, self.fs)
 
 
 if __name__ == "__main__":
